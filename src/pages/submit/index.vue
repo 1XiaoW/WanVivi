2210045e6de606043b098e9ad6db6a81975e82f9
linxiaowan299 <1604808220@qq.com>
2024-01-09 21:02:05 +0800

    完成视频投稿功能的优化

--- src/pages/submit/index.vue
+++ src/pages/submit/index.vue
@@ -2,17 +2,32 @@
 import { UploadFilled } from '@element-plus/icons-vue';
 import { reqSubmitVideo } from '@/api/video/index.ts';
 import { ref, onMounted, reactive } from 'vue';
+import { Plus } from '@element-plus/icons-vue';
 import useChannelStore from '@/store/modules/channel.ts';
+import { genFileId } from 'element-plus';
+import { ElMessage } from 'element-plus';
+
+interface VideoForm {
+  videoTitle: string;
+  videoBrief: string;
+  videoAreaId: number;
+  videoAreaName: string;
+}
 
 let useChannel = useChannelStore();
 
 let activeIndex = ref<string>('0');
 
-let files = ref();
-let addForm = reactive({
+const imageUrl = ref('');
+const files = ref();
+const coverImg = ref();
+const formRef = ref();
+const uploadRef = ref();
+const loading = ref(false);
+let addForm: VideoForm = reactive({
   videoTitle: '1',
   videoBrief: '2',
-  videoAreaId: 1027,
+  videoAreaId: 0,
   videoAreaName: '',
 });
 const props = {
@@ -24,33 +39,94 @@ onMounted(async () => {
   await useChannel.getChannelList();
 });
 
+const rules = {
+  videoTitle: [
+    { required: true, message: '请输入视频标题', trigger: 'blur' },
+    { max: 30, message: '视频标题不能超过30个字符', trigger: 'blur' },
+  ],
+  videoBrief: [
+    { required: true, message: '请输入视频简介', trigger: 'blur' },
+    { max: 200, message: '视频简介不能超过200个字符', trigger: 'blur' },
+  ],
+  videoAreaId: [
+    {
+      type: 'array',
+      required: true,
+      message: '请选择视频分区',
+      trigger: 'change',
+    },
+  ],
+};
+
+// 视频上传on-change
 const upldchange = (file: any) => {
   if (file.raw.type.startsWith('video/')) {
     // 处理视频文件
     files.value = file.raw;
+    activeIndex.value = '1';
   } else {
-    // 处理图片文件
+    // 处理其他文件
     // ...
-    console.log('你上传的不是视频啊');
+    ElMessage.error('你上传的不是视频啊');
+    uploadRef.value.clearFiles();
+  }
+};
+// 视频文件上传限制
+const handleExceed = (files: any) => {
+  uploadRef.value!.clearFiles();
+  const file = files[0];
+  file.uid = genFileId();
+  uploadRef.value!.handleStart(file);
+};
+
+// 上传封面
+const coverChange = async (file: any) => {
+  if (file.raw.type.startsWith('image/')) {
+    // 处理图片文件
+    imageUrl.value = URL.createObjectURL(file.raw!);
+    coverImg.value = file.raw;
+    activeIndex.value = '2';
+  } else {
+    // 处理其他文件
+    ElMessage.error('你上传的不是图片啊');
   }
 };
 
+// 最终投稿
 const submitupload = async () => {
-  addForm.videoAreaName = useChannel.channelList.find(
-    (v) => v.id == addForm.videoAreaId
-  )?.channel!;
-  if (files.value?.length !== 0) {
-    let formData = new FormData();
-    for (const key in addForm) {
-      // @ts-ignore
-      formData.append(key, addForm[key]);
+  formRef.value.validate(async (valid: any) => {
+    if (valid) {
+      // 校验通过，执行提交操作
+      addForm.videoAreaName = useChannel.channelList.find(
+        (v) => v.id == addForm.videoAreaId
+      )?.channel!;
+      if (files.value === undefined) {
+        activeIndex.value = '0';
+        ElMessage.error('请选择要投稿的视频');
+      } else if (coverImg.value === undefined) {
+        activeIndex.value = '1';
+        ElMessage.error('请选择要投稿的封面');
+      } else {
+        loading.value = true;
+        let formData = new FormData();
+        for (const key in addForm) {
+          const value = addForm[key as keyof VideoForm].toString();
+          formData.append(key, value);
+        }
+        formData.append('file', files.value);
+        formData.append('coverImg', coverImg.value);
+        const res = await reqSubmitVideo(formData);
+        if (res.status === 200) {
+          loading.value = false;
+          activeIndex.value = '3';
+          ElMessage.success('视频投稿成功');
+        }
+      }
+    } else {
+      // 校验失败，提示错误信息
+      ElMessage.error('提交失败');
     }
-    formData.append('file', files.value);
-    let result = await reqSubmitVideo(formData);
-    console.log(result);
-  } else {
-    // ...
-  }
+  });
 };
 </script>
 
@@ -59,14 +135,12 @@ const submitupload = async () => {
     <WanViviTop />
     <div class="content">
       <!-- 卡片视图 -->
-      <el-card class="sumbitCard">
+      <el-card
+        class="sumbitCard"
+        v-loading="loading"
+        element-loading-text="正在上传中，请勿关闭或刷新此页面">
         <!-- 提示区 -->
-        <el-alert
-          title="投稿视频"
-          type="success"
-          center
-          show-icon
-          :closable="false">
+        <el-alert title="投稿视频" type="success" center :closable="false">
         </el-alert>
         <!-- 步骤条 -->
         <el-steps
@@ -74,15 +148,16 @@ const submitupload = async () => {
           finish-status="success"
           align-center="">
           <el-step title="上传视频"></el-step>
+          <el-step title="视频封面"></el-step>
           <el-step title="基本信息"></el-step>
-          <el-step title="完成"></el-step>
         </el-steps>
 
         <!-- tab栏区域 -->
         <el-form
-          ref="addFormRef"
+          ref="formRef"
           label-width="100px"
           label-position="top"
+          :rules="rules"
           :model="addForm">
           <el-tabs
             v-model="activeIndex"
@@ -92,27 +167,39 @@ const submitupload = async () => {
               <!-- 渲染表单的Item项 -->
               <el-form-item>
                 <el-upload
+                  ref="uploadRef"
                   class="upload-video"
-                  action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
                   drag
                   accept="video/*"
+                  :limit="1"
                   :on-change="upldchange"
+                  :on-exceed="handleExceed"
                   :auto-upload="false">
                   <el-icon class="el-icon--upload"><upload-filled /></el-icon>
                   <div class="el-upload__text">
                     拖拽此处或 <em>点击上传</em>
                   </div>
-                  <template #tip>
+                  <!-- <template #tip>
                     <div class="el-upload__tip">
                       jpg/png files with a size less than 500kb
                     </div>
-                  </template>
+                  </template> -->
                 </el-upload>
               </el-form-item>
             </el-tab-pane>
             <el-tab-pane label="视频封面" name="1">
               <el-form-item label="封面图">
-                <el-input type="number"></el-input>
+                <el-upload
+                  class="upload-cover"
+                  :show-file-list="false"
+                  :on-change="coverChange"
+                  :auto-upload="false"
+                  accept="image/*">
+                  <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+                  <el-icon v-else class="avatar-uploader-icon"
+                    ><Plus
+                  /></el-icon>
+                </el-upload>
               </el-form-item>
             </el-tab-pane>
             <el-tab-pane label="基本信息" name="2">
@@ -133,7 +220,7 @@ const submitupload = async () => {
                   show-word-limit
                   v-model="addForm.videoBrief"></el-input>
               </el-form-item>
-              <el-form-item label="视频分区" prop="videoArea">
+              <el-form-item label="视频分区" prop="videoAreaId">
                 <el-cascader
                   v-model="addForm.videoAreaId"
                   :options="useChannel.channelList"
@@ -161,6 +248,23 @@ const submitupload = async () => {
       .upload-tabs {
         .upload-video {
           width: 100%;
+        }
+        .upload-cover,
+        .avatar {
+          width: 178px;
+          height: 178px;
+          display: block;
+          border-radius: 5px;
+          border: 1px dashed #dcdfe6;
+          &:hover {
+            border: 1px dashed #409eff;
+          }
+        }
+        .upload-success {
+          margin-top: 10%;
+          font-size: 28px;
+          color: #67c23a;
+          text-align: center;
         }
       }
     }
